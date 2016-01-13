6e5548db4fe02bf403baaeb90c6cfd792715911b
Jack Yeh <jack.yeh@gmail.com>
2016-01-12 21:02:10 -0700

    add functionality: usernames are displayed in the chat app - server: create usernames functionality - server: update usernames functionality - server: disconnect users when page is reloaded - styling: move the display none styling from #namesWrapper to #mainWrapper - client: create usernames functionality and array - client: update usernames - client: update send messages to pass through the username of sender - client: create disconnect functionality - client: create username submit functionality - client: create usernames to be displayed functionality - client: append messages with username of sender inside chat window

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server);
+    usernames = [];
 
 server.listen(process.env.PORT || 3000);
 
@@ -10,8 +11,32 @@ app.get('/', function(request, response){
 });
 
 io.sockets.on('connection', function(socket){
+  // create usernames
+    socket.on('new user', function(data, callback){
+      if (usernames.indexOf(data) != -1) {
+        callback(false);
+      } else {
+        callback(true);
+        socket.username = data;
+        usernames.push(socket.username);
+        updateUsernames();
+      }
+    });
+
+  // update usernames
+    function updateUsernames() {
+      io.sockets.emit('usernames', usernames);
+    }
+
   // send message
     socket.on('send message', function(data){
-        io.sockets.emit('new message', { msg: data });
+        io.sockets.emit('new message', { msg: data, user: socket.username  });
+    });
+
+  // disconnect
+    socket.on('disconnect', function(data) {
+      if (!socket.username) return;
+      usernames.splice(usernames.indexOf(socket.username), 1);
+      updateUsernames();
     });
 });
